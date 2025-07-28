31fa84f2b829233b6270fd205b1999f0c732ac7d
unknown <20223020128@estudantes.ifpr.edu.br>
2025-07-28 15:53:28 -0300

    Controle de acesso às views com LoginRequiredMixin e Adição do Framework de Mensagens com SuccessMessageMixin

--- paginas/views.py
+++ paginas/views.py
@@ -27,41 +27,45 @@ class SobreView(TemplateView):
 
 #########################   CREATE   ##################################
 
-class CategoriaCreate(LoginRequiredMixin, CreateView):
+class CategoriaCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Categoria
     template_name = 'paginas/form.html'
     fields = ['nome']
     success_url = reverse_lazy('index')
+    success_message = "Categoria criada com sucesso!"
     extra_context = {
         'titulo': 'Nova Categoria',
         'botao': 'Criar Categoria',
     }
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Post
     template_name = 'paginas/form.html'
     fields = ['titulo',  'texto', 'categoria', 'autor']
     success_url = reverse_lazy('index')
+    success_message = "Post criado com sucesso!"
     extra_context = {
         'titulo': 'Novo Post',
         'botao': 'Publicar Post'
     }
 
-class AvaliacaoCreate(CreateView):
+class AvaliacaoCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Avaliação
     template_name = 'paginas/form.html'
     fields = ['autor', 'nota', 'post']
     success_url = reverse_lazy('index')
+    success_message = "Avaliação criada com sucesso!"
     extra_context = {
         'titulo': 'Nova Avaliação',
         'botao': 'Avaliar'
     }
 
-class ComentarioCreate(CreateView):
+class ComentarioCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Comentario
     template_name = 'paginas/form.html'
     fields = ['autor', 'comentario', 'post']
     success_url = reverse_lazy('index')
+    success_message = "Comentário criado com sucesso!"
     extra_context = {
         'titulo': 'Novo Comentário',
         'botao': 'Publicar Comentário'
@@ -70,41 +74,45 @@ class ComentarioCreate(CreateView):
 
 #########################   UPDATE   ##################################
 
-class CategoriaUpdate(UpdateView):
+class CategoriaUpdate(LoginRequiredMixin,SuccessMessageMixin,  UpdateView):
     model = Categoria
     template_name = 'paginas/form.html'
     fields = ['nome']
     success_url = reverse_lazy('index')
+    success_message = "Categoria editada com sucesso!"
     extra_context = {
         'titulo': 'Editar Categoria',
         'botao': 'Atualizar Categoria',
     }
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Post
     template_name = 'paginas/form.html'
     fields = ['titulo',  'texto', 'categoria', 'autor']
     success_url = reverse_lazy('index')
+    success_message = "Post editado com sucesso!"
     extra_context = {
         'titulo': 'Editar Post',
         'botao': 'Atualizar Post'
     }
 
-class AvaliacaoUpdate(UpdateView):
+class AvaliacaoUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Avaliação
     template_name = 'paginas/form.html'
     fields = ['autor', 'nota', 'post']
     success_url = reverse_lazy('index')
+    success_message = "Avaliação editada com sucesso!"
     extra_context = {
         'titulo': 'Editar Avaliação',
         'botao': 'Atualizar Avaliação'
     }
 
-class ComentarioUpdate(UpdateView):
+class ComentarioUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Comentario
     template_name = 'paginas/form.html'
     fields = ['autor', 'comentario', 'post']
     success_url = reverse_lazy('index')
+    success_message = "Comentário editado com sucesso!"
     extra_context = {
         'titulo': 'Editar Comentário',
         'botao': 'Atualizar Comentário'
@@ -112,37 +120,41 @@ class ComentarioUpdate(UpdateView):
 
 #########################   DELETE   ##################################
 
-class CategoriaDelete(DeleteView):
+class CategoriaDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Categoria
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('index')
+    success_message = "Categoria excluida com sucesso!"
     extra_context = {
         'titulo': 'Excluir Categoria',
         'botao': 'Excluir Categoria',
     }
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Post
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('index')
+    success_message = "Post excluido com sucesso!"
     extra_context = {
         'titulo': 'Excluir Post',
         'botao': 'Excluir Post',
     }
 
-class AvaliacaoDelete(DeleteView):
+class AvaliacaoDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Avaliação
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('index')
+    success_message = "Avaliação excluida com sucesso!"
     extra_context = {
         'titulo': 'Excluir Avaliação',
         'botao': 'Excluir Avaliação',
     }
 
-class ComentarioDelete(DeleteView):
+class ComentarioDelete(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Comentario
     template_name = 'paginas/form.html'
     success_url = reverse_lazy('index')
+    success_message = "Comentario excluido com sucesso!"
     extra_context = {
         'titulo': 'Excluir Comentário',
         'botao': 'Excluir Comentário',
@@ -152,7 +164,7 @@ class ComentarioDelete(DeleteView):
 ##################################################
 
 
-class CategoriaList(ListView):
+class CategoriaList(LoginRequiredMixin, ListView):
     model = Categoria
     template_name = 'paginas/listas/categoria.html'
 
@@ -168,47 +180,3 @@ class ComentarioList(ListView):
     model = Comentario
     template_name = 'paginas/listas/comentario.html'
 
-    ##################################################
-
-    class CategoriaList(LoginRequiredMixin, ListView):
-     model = Categoria
-     template_name = 'categoria'
-
-    class PostList(LoginRequiredMixin, ListView):
-     model = Post
-     template_name = 'post'
-
-    class AvaliaçãoList(LoginRequiredMixin, ListView):
-     model = Avaliação
-     template_name = 'avaliação'
-
-    class ComentarioList(LoginRequiredMixin, ListView):
-     model = Comentario
-     template_name = 'comentario'
-
-    #####################################################
-
-    class CriarCategoriaView(SuccessMessageMixin, CreateView):
-      model = Categoria
-      fields = ['nome']
-      success_url = '/alunos/'
-      success_message = "Categoria criada com sucesso!"
-
-    class CriarPostView(SuccessMessageMixin, CreateView):
-      model = Post
-      fields = ['titulo',  'texto', 'categoria', 'autor']
-      success_url = '/alunos/'
-      success_message = "Post criado com sucesso!"
-
-
-    class CriarAvaliaçãoView(SuccessMessageMixin, CreateView):
-      model = Avaliação
-      fields =  ['autor', 'nota', 'post']
-      success_url = '/alunos/'
-      success_message = "Avaliação criada com sucesso!"
-
-    class CriarComentarioView(SuccessMessageMixin, CreateView):
-      model = Comentario
-      fields =  ['autor', 'comentario', 'post']
-      success_url = '/alunos/'
-      success_message = "Comentario criado com sucesso!"
